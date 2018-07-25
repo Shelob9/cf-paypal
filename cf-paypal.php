e43593ebd1473eb14db680893bbb4537852b35ad
Josh Pollock <jpollock412@gmail.com>
2018-07-25 12:33:51 -0400

    Change author name and remove licensing code.
    I don't love this, but GPL. But please do your best to avoid confusion with our paid product.

--- cf-paypal.php
+++ cf-paypal.php
@@ -1,14 +1,13 @@
 <?php
 /**
  *
- * Copyright 2014 -2017 David Cramer and CalderaWP LLC
  *
  * @wordpress-plugin
  * Plugin Name: Caldera Forms PayPal
  * Plugin URI: https://github.com/misfist/cf-paypal
  * Description: PayPal checkout processor for Caldera Forms.
  * Version: 1.1.7
- * Author:      Caldera Labs
+ * Author:      Misfit
  * Author URI:  https://github.com/misfist
  * Text Domain: cf-paypal
  * License:     GPL-3.0
@@ -37,9 +36,6 @@ add_filter('caldera_forms_submit_return_transient_pre_process', 'cf_paypal_expre
 // filter to add PayPal auth redirect
 add_filter('caldera_forms_submit_return_redirect-paypal_express', 'cf_paypal_express_redirect_topaypal', 10, 4);
 
-// filter to initialize the license system
-//add_action( 'init', 'cf_paypal_express_init_license' );
-
 // load dependencies
 include_once CF_PAYPAL_EXPRESS_PATH . 'vendor/autoload.php';
 
@@ -47,21 +43,3 @@ include_once CF_PAYPAL_EXPRESS_PATH . 'vendor/autoload.php';
 include CF_PAYPAL_EXPRESS_PATH . 'includes/functions.php';
 
 
-function cf_paypal_express_init_license() {
-	$plugin = array(
-		'name'      => 'Caldera Forms PayPal Express Add-on',
-		'slug'      => 'caldera-forms-paypal-express-add-on',
-		'url'       => 'https://calderawp.com',
-		'version'   => CF_PAYPAL_EXPRESS_VER,
-		'key_store' => 'cf_paypal_express_license',
-		'file'      => __FILE__
-	);
-
-	if ( is_admin() || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
-		include_once CF_PAYPAL_EXPRESS_PATH . 'vendor/calderawp/dismissible-notice/src/functions.php';
-	}
-
-	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-	new \calderawp\licensing_helper\licensing( $plugin );
-
-}
